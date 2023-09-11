dd86af5f46985ab240105b4b699739744f14d49f
Fuad Miftah <fuadmiftah9@gmail.com>
2023-09-11 11:41:13 +0300

    remove-unwanted-controller-implementation

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -4,134 +4,20 @@ import { createError } from '../utils/error.js';
 
 // Add a book to the cart
 export const addToCart = async (req, res, next) => {
-    try {
-        const { clientId, bookId, quantity } = req.body;
 
-        // Validate input (e.g., ensure quantity is a positive integer)
-
-        // Find the client by clientId
-        const client = await Client.findById(clientId);
-
-        if (!client) {
-            return next(createError(404, 'Client not found'));
-        }
-
-        // Check if the bookId is valid
-        const book = await Book.findById(bookId);
-
-        if (!book) {
-            return next(createError(404, 'Book not found'));
-        }
-
-        // Check if the book is already in the cart
-        const existingCartItemIndex = client.cart.findIndex(
-            (item) => item.bookId.toString() === bookId
-        );
-
-        if (existingCartItemIndex !== -1) {
-            // If the book is already in the cart, update the quantity
-            client.cart[existingCartItemIndex].quantity += quantity;
-        } else {
-            // If not, add the book to the cart
-            client.cart.push({
-                bookId,
-                quantity,
-                totalPrice: book.price * quantity, // Calculate the total price
-            });
-        }
-
-        // Save the updated client document
-        await client.save();
-
-        res.status(200).json(client.cart);
-    } catch (error) {
-        next(error);
-    }
 };
 
 // Update the quantity of a book in the cart
 export const updateCartItemQuantity = async (req, res, next) => {
-    try {
-        const { clientId, bookId, quantity } = req.body;
 
-        // Validate input (e.g., ensure quantity is a positive integer)
-
-        // Find the client by clientId
-        const client = await Client.findById(clientId);
-
-        if (!client) {
-            return next(createError(404, 'Client not found'));
-        }
-
-        // Find the index of the book in the cart
-        const cartItemIndex = client.cart.findIndex(
-            (item) => item.bookId.toString() === bookId
-        );
-
-        if (cartItemIndex === -1) {
-            return next(createError(404, 'Book not found in the cart'));
-        }
-
-        // Update the quantity and total price
-        client.cart[cartItemIndex].quantity = quantity;
-        client.cart[cartItemIndex].totalPrice = client.cart[cartItemIndex].quantity * book.price;
-
-        // Save the updated client document
-        await client.save();
-
-        res.status(200).json(client.cart);
-    } catch (error) {
-        next(error);
-    }
 };
 
 // Remove a book from the cart
 export const removeFromCart = async (req, res, next) => {
-    try {
-        const { clientId, bookId } = req.params;
 
-        // Find the client by clientId
-        const client = await Client.findById(clientId);
-
-        if (!client) {
-            return next(createError(404, 'Client not found'));
-        }
-
-        // Find the index of the book in the cart
-        const cartItemIndex = client.cart.findIndex(
-            (item) => item.bookId.toString() === bookId
-        );
-
-        if (cartItemIndex === -1) {
-            return next(createError(404, 'Book not found in the cart'));
-        }
-
-        // Remove the book from the cart
-        client.cart.splice(cartItemIndex, 1);
-
-        // Save the updated client document
-        await client.save();
-
-        res.status(204).send(); // Respond with a success status code
-    } catch (error) {
-        next(error);
-    }
 };
 
 // View the cart contents
 export const viewCart = async (req, res, next) => {
-    try {
-        const { clientId } = req.params;
 
-        // Find the client by clientId
-        const client = await Client.findById(clientId).populate('cart.bookId');
-
-        if (!client) {
-            return next(createError(404, 'Client not found'));
-        }
-
-        res.status(200).json(client.cart);
-    } catch (error) {
-        next(error);
-    }
 };
--- server/controllers/order.js
+++ server/controllers/order.js
@@ -2,87 +2,25 @@ import Order from '../models/Order.js';
 
 // Create a new order
 export const createOrder = async (req, res, next) => {
-    try {
-        const { userId, bookIds, address, postalCode, totalPrice } = req.body;
 
-        // Validate input data
-
-        const newOrder = new Order({
-            userId,
-            bookId: bookIds, // Assuming bookIds is an array of book IDs
-            address,
-            postalCode,
-            orderStatus: 'Processing', // Default status when creating an order
-            totalPrice,
-        });
-
-        const savedOrder = await newOrder.save();
-        res.status(201).json(savedOrder);
-    } catch (error) {
-        next(error);
-    }
 };
 
 // Update the order status
 export const updateOrderStatus = async (req, res, next) => {
-    try {
-        const { orderId } = req.params;
-        const { orderStatus } = req.body;
 
-        // Validate input data and check if orderStatus is a valid value
-
-        const updatedOrder = await Order.findByIdAndUpdate(
-            orderId,
-            { $set: { orderStatus } },
-            { new: true }
-        );
-
-        if (!updatedOrder) {
-            return res.status(404).json({ message: 'Order not found' });
-        }
-
-        res.status(200).json(updatedOrder);
-    } catch (error) {
-        next(error);
-    }
 };
 
 // Get a single order by ID
 export const getOrderById = async (req, res, next) => {
-    try {
-        const { orderId } = req.params;
-        const order = await Order.findById(orderId);
 
-        if (!order) {
-            return res.status(404).json({ message: 'Order not found' });
-        }
-
-        res.status(200).json(order);
-    } catch (error) {
-        next(error);
-    }
 };
 
 // Get all orders
 export const getAllOrders = async (req, res, next) => {
-    try {
-        const orders = await Order.find();
-        res.status(200).json(orders);
-    } catch (error) {
-        next(error);
-    }
+
 };
 
 // Get orders by client user ID
 export const getClientOrders = async (req, res, next) => {
-    try {
-        const { clientId } = req.params;
 
-        // Find orders where the userId matches the client's user ID
-        const orders = await Order.find({ userId: clientId });
-
-        res.status(200).json(orders);
-    } catch (error) {
-        next(error);
-    }
 };
