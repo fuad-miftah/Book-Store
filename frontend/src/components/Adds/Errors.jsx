12d06acfa18353ef5b7efff1422e4f8ded60a71c
HaileamlakWaleligne <hailemalakwaleligne3910@gmail.com>
2023-10-15 02:04:34 +0300

    adding error section

--- frontend/src/components/Adds/Errors.jsx
+++ frontend/src/components/Adds/Errors.jsx
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import error from './404.png';
 
-const Error = (props) => {
+const Errors = (props) => {
   const { errors } = props;
 
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 h-screen my-10'>
-      <div className="flex flex-col justify-center items-start px-20">
-        <p className="text-red-400 font-semibold text-lg">{errors}</p>
-        <p className="font-bold text-5xl">
+      <div className='flex flex-col justify-center items-start px-20'>
+        <p className='text-red-400 font-semibold text-lg'>{errors}</p>
+        <p className='font-bold text-5xl'>
           Whoops! This is not <br />
           what you were looking
           <br />
           for
         </p>
-        <p className="text-lg font-medium">
+        <p className='text-lg font-medium'>
           but you just found the sock we&apos;d lost, thanks. <br />
-          Try your luck by going back to the <Link to='/' className="list-none">Home page</Link>
+          Try your luck by going back to the{' '}
+          <Link to='/' className='list-none'>
+            Home page
+          </Link>
         </p>
       </div>
       <div>
@@ -28,8 +31,8 @@ const Error = (props) => {
   );
 };
 
-Error.propTypes = {
+Errors.propTypes = {
   errors: PropTypes.string.isRequired,
 };
 
-export default Error;
+export default Errors;
